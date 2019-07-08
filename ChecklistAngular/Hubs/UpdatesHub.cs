ae38ee4d55fa0392c507bf1cd0e584361cc6cc2d
Tom Yee <tom.yee713@gmail.com>
2019-07-08 16:13:56 -0400

    added history trail to update hub

--- ChecklistAngular/Hubs/UpdatesHub.cs
+++ ChecklistAngular/Hubs/UpdatesHub.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace ChecklistAngular.Hubs
@@ -23,7 +24,23 @@ namespace ChecklistAngular.Hubs
         public async Task GetProgress(int id, int stepNum, string action)
         {
             var step = await _repo.GetUpdateSteps(stepNum, id);
+            var status = await _repo.GetUpdate(step.Idupdate);
             step.Progress = action;
+            switch (action)
+            {
+                case "Done":
+                    
+                    LogHistory(step, status.Status, "Completed Step ");
+                    break;
+                case "Skip":
+                    step.Progress = "Skip";
+                    LogHistory(step, status.Status, "Skipped Step ");
+                    break;
+                case "":
+                    step.Progress = "";
+                    LogHistory(step, status.Status, "Unchecked Step ");
+                    break;
+            }
             await _repo.SaveAll();
             await Clients.All.SendAsync("StepProgress", step.Progress, step.Step, step.Comment);
         }
@@ -34,6 +51,22 @@ namespace ChecklistAngular.Hubs
             step.Comment = comment;
             await _repo.SaveAll();
             await Clients.All.SendAsync("StepProgress", step.Progress, step.Step, step.Comment);
+
+        }
+
+        private void LogHistory(LogUpdateSteps step, string status, string action)
+        {
+            var user = WindowsIdentity.GetCurrent().Name;
+            var history = new LogUpdateHistory
+            {
+                Idupdate = step.Idupdate,
+                FileTime = DateTime.Now,
+                FileBy = user.Substring(user.IndexOf(@"\") + 1),
+                Status = status,
+                FileAction = action + step.Step
+            };
+
+            _repo.Add(history);
 
         }
 
